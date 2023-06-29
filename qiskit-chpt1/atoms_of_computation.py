b837d2c22615fe2893fad3b10d8461718f38d3e2
Melody Lee <melody.lee357@gmail.com>
2023-06-29 19:37:28 -0400

    updates

---
+++ qiskit-chpt1/atoms_of_computation.py
@@ -0,0 +1,8 @@
+# from qiskit import QuantumCircuit, assemble, Aer
+# from qiskit.visualization import plot_histogram
+
+from numpy import array
+ket0 = array([1,0])
+ket1 = array([0,1])
+
+display(ket0/2+ket1/2)
